6ebefcbd164b0cea1e525bba8173109a454266dc
Meloe <meloscavezzoni@gmail.com>
2019-07-04 11:55:55 +0200

    ajout win

--- javascript.js
+++ javascript.js
@@ -10,6 +10,8 @@ let tabWidth = widthCanvas/3;         // largueur
 let tabHeight = heightCanvas/3;       // hauteur
 let tabPadding = 5;                   // padding
 
+let isPlaying = true;
+
 function tab(a){
   // Boucle pour créer le tableau à deux dimensions
   let tableau = [];     
@@ -24,39 +26,6 @@ function tab(a){
 
 let tableau = tab({ x: 0, y: 0 });    // on ajoute l'objet
 let oxo = tab("");                    // variable avec champ vide pour vérifier s'il y a un X ou O dans la case
-
-// Evenements
-let tourX = true;
-/*let nbr = Math.floor(Math.random()*(3-1)+1);*/
-
-document.getElementById("joueur1").style.display = "block";
-
-document.addEventListener("click", (e)=>{
-  // On récupère l'endroit où l'on clic en X et Y
-  let x = Math.floor((e.clientX - canvas.offsetLeft) / (widthCanvas / 3)); 
-  let y = Math.floor((e.clientY - canvas.offsetTop) / (heightCanvas / 3));
-
-  if(tourX === true){
-    if(oxo[y][x] === ""){
-      drawX(x,y);
-      oxo[y][x] = "x";
-      tourX = false;
-      document.getElementById("joueur1").style.display = "none";    // Une fois que le joueur 1 à joué, il disparaît et le joueur 2 apparaît
-      document.getElementById("joueur2").style.display = "block";
-    }
-  }
-  else{
-    if(oxo[y][x] === ""){
-      drawO(x,y);
-      oxo[y][x] = "o";
-      tourX = true;
-      document.getElementById("joueur1").style.display = "block";
-      document.getElementById("joueur2").style.display = "none";
-    }
-  }
-  console.log(oxo);
-
-}, false);
 
 // Fonction pour boucler les éléments dans le tableau et les dessiner
 function drawElement() {
@@ -90,15 +59,90 @@ function drawO(x,y) {               // On dessine le O
   context.fillText("O", x*tabWidth+(tabWidth/4), (y+1)*tabHeight-(tabHeight/4));    // a modif
 }
 
-function verification(){
-  for(let c=0; c < tabColumnCount; c++) {       // colonnes
-    for(let r=0; r < tabRowCount; r++) {       // lignes
-        if(oxo == ["X","",""]){
-          console.log("ok");
+function verifColumnAndRow(oxoTableau){
+    // On fait une ligne pour vérifier les colonnes puis les lignes
+    for(let i=0; i< oxoTableau.length; i++){
+        if(oxoTableau[0][i] != "" && oxoTableau[0][i] === oxoTableau[1][i] && oxoTableau[0][i] === oxoTableau[2][i]){
+            return true;
+        }
+        if(oxoTableau[i][0] != "" && oxoTableau[i][0] === oxoTableau[i][1] && oxoTableau[i][0] === oxoTableau[i][2]){
+            return true;
         }
     }
-  }
+    return false;
 }
-verification();
+function verifDia(oxoTableau){
+    // On vérifie les diagonales
+    if(oxoTableau[0][0] != "" && oxoTableau[0][0] === oxoTableau[1][1] && oxoTableau[0][0] === oxoTableau[2][2]){
+        return true;
+    }
+    else if(oxoTableau[0][2] != "" && oxoTableau[0][2] === oxoTableau[1][1] && oxoTableau[0][2] === oxoTableau[2][0]){
+        return true;
+    }
+    else{ 
+      return false;
+    }
+}
+
+function verification(oxoTableau){
+    return verifDia(oxoTableau) ||verifColumnAndRow(oxoTableau);
+}
 
 drawElement();
+
+
+// EVENEMENTS
+/*let nbr = Math.floor((Math.random()*10)%2);*/
+let tourX = true;
+document.getElementById("joueur1").style.display = "block";
+
+document.addEventListener("click", (e)=>{
+  // On récupère l'endroit où l'on clic en X et Y
+  let x = Math.floor((e.clientX - canvas.offsetLeft) / (widthCanvas / 3)); 
+  let y = Math.floor((e.clientY - canvas.offsetTop) / (heightCanvas / 3));
+
+  /*if(nbr === 0){
+    tourX = true;
+    nbr=1;
+  }
+  else{
+    tourX = false;
+    nbr = 0;
+  }*/
+
+  if(tourX == true){
+    if(oxo[y][x] === ""){
+      drawX(x,y);
+      oxo[y][x] = "X";
+      tourX = false;
+      document.getElementById("joueur1").style.display = "none";    // Une fois que le joueur 1 a joué, il disparaît et le joueur 2 apparaît
+      document.getElementById("joueur2").style.display = "block";
+    }
+  }
+  else{
+    if(oxo[y][x] === ""){
+      drawO(x,y);
+      oxo[y][x] = "0";
+      tourX = true;
+      document.getElementById("joueur1").style.display = "block";
+      document.getElementById("joueur2").style.display = "none";
+    }
+  }
+  
+function verifEnd(){
+  if(verification(oxo) == true){
+    if(oxo[y][x] === "X"){
+      alert("Le joueur 1 gagne !");
+      document.location.reload();
+    }
+    else{
+      alert("Le joueur 2 gagne !");
+      document.location.reload();
+    }
+}
+}
+setTimeout(function(){verifEnd()},500); 
+
+  console.log(oxo);
+  console.log(verification(oxo));
+}, false);
